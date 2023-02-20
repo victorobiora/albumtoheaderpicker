3397436e2fa153e136a338ffe07a3ab5cfef1d33
Victor <victorobioratech@gmail.com>
2023-02-20 13:12:01 +0100

    'little changes'

--- src/components/header/ImageContainer.js
+++ src/components/header/ImageContainer.js
@@ -8,9 +8,9 @@ const ImageContainer = props => {
     return   <Fragment>
 <div className= {classes["imageContainer"]}>
    <div className= {classes["header-image"]} >
-    <img src={props.image} alt="CHOOSE GREYSPRING TODAY"/>
+    <img src={props.image} alt="CHOOSE REALTIME TODAY"/>
     </div>
-    <h1>GreySpring Music</h1> 
+    <h1>RealTime Music</h1> 
 </div>
     </Fragment>   
 
--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -3,7 +3,7 @@ import classes from './headernav.module.css'
 const Nav = props => {
     return  (
     <nav className={classes.nav}>
-    <div className={classes["nav-home"]} > GreySpring </div>
+    <div className={classes["nav-home"]} > RealTime </div>
     <ul className={classes["nav-home__links"]}>
         <li>Newsletter</li>
         <li>Charts</li>
